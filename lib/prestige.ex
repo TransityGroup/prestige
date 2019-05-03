2ccb58e807606274e0fedec4924a31c9dbabb14a
Brian Balser <brian.balser@gmail.com>
2019-05-02 21:21:35 -0400

    Handle bad requests and errors reported by presto

--- lib/prestige.ex
+++ lib/prestige.ex
@@ -2,6 +2,15 @@ defmodule Prestige do
   @moduledoc """
   An elixir client for [Prestodb](http://prestodb.github.io/).
   """
+
+  defmodule Error do
+    defexception [:message, :code, :location, :name, :type, :stack]
+  end
+
+  defmodule BadRequestError do
+    defexception [:message]
+  end
+
   defdelegate execute(statement, opts \\ []), to: Prestige.Statement
 
   defdelegate prefetch(result), to: Prestige.Statement
