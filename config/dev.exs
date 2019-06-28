0525cb4e79451f5199e3f452b59a9784facc2e9c
Richard Jones <rajones@hntb.com>
2019-06-28 16:00:40 -0400

    Add pre commit hook script to check credo, format, and deps versions
    co-authored-by: Scott Millard <smillard@hntb.com>

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,4 @@ use Mix.Config
 config :prestige, base_url: "https://presto.example.com"
 
 config :husky,
-  pre_commit: "mix format --check-formatted && mix credo && mix hex.outdated"
+  pre_commit: "./scripts/git_pre_commit_hook.sh"
