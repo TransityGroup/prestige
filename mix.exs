d9abf12f644d5c3233104f19318b97171f9bba0e
Brian Balser <brian.balser@gmail.com>
2020-01-28 15:20:14 -0500

    Bumped version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Prestige.MixProject do
   def project do
     [
       app: :prestige,
-      version: "1.0.2",
+      version: "1.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
